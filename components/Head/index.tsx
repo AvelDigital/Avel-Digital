706c71ca0d3c4b8e4729d5ad2752519d66f84b3f
Miguel Penha <miguel.penha1@gmail.com>
2024-08-21 22:40:26 -0300

    Add GTM

---
+++ components/Head/index.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+import HeadRaw from 'next/head'
+import GoogleTagManager from './GoogleTagManager'
+
+interface IProps {
+    url?: string
+    title?: string
+    image?: string
+    author?: string
+    keywords?: string
+    description?: string
+}
+
+const Head: FC<IProps> = ({ title='Avel Digital', description='Geramos vendas, você colhe os resultados', url, image='/img/thumbnail.png', author='Avel Digital', keywords='marketing digital, aumentar faturamento online, otimização de conversão, consultoria de marketing digital, agência de marketing, Avel Digital marketing' }) => {
+    return (
+        <>
+            <HeadRaw>
+                <title>{title}</title>
+                <meta name="description" content={description}/>
+                <meta property="og:title" content={title}/>
+                <meta property="og:description" content={description}/>
+                <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN+url}/>
+                <meta property="og:image" content={image}/>
+                <meta name="author" content={author}/>
+                <meta name="keywords" content={keywords}/>
+            </HeadRaw>
+            <GoogleTagManager/>
+        </>
+    )
+}
+
+export default Head
