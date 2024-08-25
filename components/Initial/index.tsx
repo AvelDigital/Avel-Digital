834c1717f492333f73c87aa75922a3f4dd2a42de
Miguel Penha <miguel.penha1@gmail.com>
2024-08-25 01:09:08 -0300

    Add thumbnail to video

--- components/Initial/index.tsx
+++ components/Initial/index.tsx
@@ -10,7 +10,7 @@ function Initial() {
                 <Subtitle>Revolucionamos suas operações de marketing, vendas e experiência do usuário, garantindo uma marca eficaz e resultados de negócios excepcionais.</Subtitle>
                 <ButtonLink $primary="secondary" $secondary="primary" href="/contact">FALAR COM ESPECIALISTA</ButtonLink>
             </Content>
-            <Video autoPlay loop muted playsInline> 
+            <Video autoPlay loop muted playsInline poster="/img/thumbnail.jpg"> 
                 <source src="/img/video.mp4" type="video/mp4"/>
             </Video>
         </Container>
